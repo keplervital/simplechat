63a5893ff7b29a73c72768c1345559e8a85daa8b
Kepler Vital <kepler.vital@gmail.com>
2019-07-22 20:26:18 +0100

    user repository override findById

--- app/simplechat/src/main/java/com/huonix/simplechat/repositories/UserRepository.java
+++ app/simplechat/src/main/java/com/huonix/simplechat/repositories/UserRepository.java
@@ -1,5 +1,6 @@
 package com.huonix.simplechat.repositories;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.cassandra.repository.CassandraRepository;
@@ -9,9 +10,12 @@ import org.springframework.stereotype.Repository;
 import com.huonix.simplechat.models.User;
 
 @Repository
-public interface UserRepository extends CassandraRepository<User, UUID> {
+public interface UserRepository extends CassandraRepository<User, User> {
 
 	@Query("select * from users where name = ?0")
     Iterable<User> findByName(String name);
+	
+	@Query(allowFiltering = true)
+	Optional<User> findById(UUID id);
 	
 }
