c698f7756f09407c7848795c809a01c20ebf38ca
Kepler Vital <kepler.vital@gmail.com>
2019-07-24 15:27:23 +0100

    custom exceptions for user

---
+++ app/simplechat/src/main/java/com/huonix/simplechat/exceptions/UserNotFoundException.java
@@ -0,0 +1,20 @@
+package com.huonix.simplechat.exceptions;
+
+/**
+ * Throw when user not found
+ * 
+ * @author Kepler Vital
+ *
+ */
+public class UserNotFoundException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public UserNotFoundException(String message) {
+		super(message);
+	}
+	
+}
