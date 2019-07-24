8e128d0d619858fb218085d8a6c3d01806578b87
Kepler Vital <kepler.vital@gmail.com>
2019-07-24 14:46:38 +0100

    use services instead of repositories in controller

--- app/simplechat/src/main/java/com/huonix/simplechat/controllers/UserController.java
+++ app/simplechat/src/main/java/com/huonix/simplechat/controllers/UserController.java
@@ -1,14 +1,10 @@
 package com.huonix.simplechat.controllers;
 
-import java.util.Date;
 import java.util.HashMap;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
-
-import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,102 +16,106 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.datastax.driver.core.utils.UUIDs;
-import com.huonix.simplechat.models.UserByApiKey;
-import com.huonix.simplechat.models.UserByName;
 import com.huonix.simplechat.models.User;
-import com.huonix.simplechat.repositories.UserByApiKeyRepository;
-import com.huonix.simplechat.repositories.UserByNameRepository;
-import com.huonix.simplechat.repositories.UserRepository;
+import com.huonix.simplechat.services.UserService;
 
+/**
+ * User rest controller
+ * 
+ * @author Kepler Vital
+ *
+ */
 @RestController
 @RequestMapping("/user")
 public class UserController {
 	
 	@Autowired
-    private UserRepository userRepository;
+	private UserService userService;
 	
-	@Autowired
-	private UserByApiKeyRepository userByApiKeyRepository;
-	
-	@Autowired
-	private UserByNameRepository userByNameRepository;
-	
+	/**
+	 * Retrieves all users
+	 * 
+	 * @return a list of all users
+	 */
 	@RequestMapping(value = "/list", method = RequestMethod.GET)
     @ResponseBody
-    public List<User> list() {
-        List<User> users = new ArrayList<>();
-        userRepository.findAll().forEach(e->users.add(e));
-        return users;
+    public ResponseEntity<List<User>> list() {
+        return new ResponseEntity<List<User>>(userService.findAll(), HttpStatus.OK);
     }
 	
+	/**
+	 * Retrieves a user
+	 * 
+	 * @param id the user id
+	 * @return the user data
+	 */
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
     @ResponseBody
     public ResponseEntity<User> get(@PathVariable("id") UUID id) {
-		Optional<User> optional = userRepository.findById(id);
+		Optional<User> optional = userService.getById(id);
 		if(!optional.isPresent()) {
 			return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
 		} 
         return new ResponseEntity<User>(optional.get(), HttpStatus.OK);
     }
 	
+	/**
+	 * Adds a new user
+	 * 
+	 * @param user the user data
+	 * @return the new generated user id and api key to access the system
+	 */
 	@RequestMapping(value = "", method = RequestMethod.POST)
     @ResponseBody
-    public Map<String, String> create(@Valid @RequestBody User user) {
-		HashMap<String, String> response = new HashMap<>();
+    public ResponseEntity<Map<String, Object>> create(@RequestBody User user) {
+		HashMap<String, Object> response = new HashMap<>();
 		try {
-			Optional<UserByName> chatUser = userByNameRepository.findByName(user.getName());
-			if(chatUser.isPresent()) {
-				throw new Exception("The name especified already exists.");
-			}
-			user.setId(UUIDs.timeBased());
-			user.setDateAdded(new Date());
-	        user = userRepository.save(user);
-	        
-	        userByApiKeyRepository.save(new UserByApiKey(user.getAccessKey(), user.getId()));
-	        userByNameRepository.save(new UserByName(user.getName(), user.getId()));
-	        
+			user = userService.add(user);
 	        response.put("id", user.getId().toString());
 	        response.put("accessKey", user.getAccessKey());
 	    } catch(Exception e) {
-	    	response.put("error", e.getMessage());
+	    	response.put("errors", userService.getErrors());
 	    }
-        return response;
+        return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
     }
 	
+	/**
+	 * Updates an existing user
+	 * 
+	 * @param id the user id
+	 * @param user the user data
+	 * @return the user id and api key to access the system
+	 */
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
     @ResponseBody
-    public Map<String, String> update(@PathVariable("id") User user) {
-		HashMap<String, String> response = new HashMap<>();
+    public ResponseEntity<Map<String, Object>> update(@PathVariable("id") UUID id, @RequestBody User user) {
+		Map<String, Object> response = new HashMap<>();
 		try {
-			// TODO
+			user = userService.update(id, user);
+	        response.put("id", user.getId().toString());
+	        response.put("accessKey", user.getAccessKey());
 	    } catch(Exception e) {
-	    	response.put("error", e.getMessage());
+	    	response.put("errors", userService.getErrors());
 	    }
-        return response;
+        return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
     }
 	
+	/**
+	 * Deletes a system user
+	 * 
+	 * @param id the user id
+	 * @return message if the user was successfully deleted
+	 */
 	@RequestMapping(value = "/delete/id/{id}", method = RequestMethod.DELETE)
     @ResponseBody
-    public Map<String, String> delete(@PathVariable("id") UUID id) {
-		HashMap<String, String> response = new HashMap<>();
-		Optional<User> optional = userRepository.findById(id);
-		if(!optional.isPresent()) {
-			response.put("error", "User not found.");
-			return response;
-		} 
-		User user = optional.get();		
-		Optional<UserByApiKey> userByApiKey = userByApiKeyRepository.findByKey(user.getAccessKey());
-		Optional<UserByName> userByName = userByNameRepository.findByName(user.getName());
-		if(userByApiKey.isPresent()) {
-			userByApiKeyRepository.delete(userByApiKey.get());
+    public ResponseEntity<Map<String, Object>> delete(@PathVariable("id") UUID id) {
+		HashMap<String, Object> response = new HashMap<>();
+		if(userService.deleteById(id)) {
+			response.put("message", "User successfully removed.");
+		} else {
+			response.put("errors", userService.getErrors());
 		}
-		if(userByName.isPresent()) {
-			userByNameRepository.delete(userByName.get());
-		}
-		userRepository.delete(user);
-		response.put("message", "User successfully removed.");
-        return response;
+        return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
     }
 
 }
