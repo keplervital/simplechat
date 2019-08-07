26b3f0e380afd9ab1dd63c77c4fa9cb2d83e3d44
Kepler Vital <kepler.vital@gmail.com>
2019-08-07 16:32:41 +0100

    added bar open/close feature

--- web/src/app/App.js
+++ web/src/app/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { StateProvider } from './simplechat/state';
-import reducer from './store/reducers';
+import reducer, {initialStates} from './store/reducers';
 import Chat from './components/Chat';
 
 const App = () => {
     return (
-        <StateProvider initialState={{}} reducer={reducer}>
+        <StateProvider initialState={initialStates} reducer={reducer}>
            <Chat />
         </StateProvider>
     );
--- web/src/index.js
+++ web/src/index.js
@@ -1,4 +1,3 @@
-import './styles/index.css';
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app/App";
