79c5d5abddd7cf943a17bc53465ddd6824f65a1d
Kepler Vital <kepler.vital@gmail.com>
2019-08-09 18:56:29 +0100

    chat css

--- web/src/app/App.js
+++ web/src/app/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { StateProvider } from './simplechat/state';
 import reducer, {initialStates} from './store/reducers';
-import Chat from './components/Chat';
+import ChatApp from './components/ChatApp';
 
 const App = () => {
     return (
         <StateProvider initialState={initialStates} reducer={reducer}>
-           <Chat />
+           <ChatApp />
         </StateProvider>
     );
 }
