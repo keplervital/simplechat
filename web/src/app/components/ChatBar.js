a9109629dd271894079ab6f51fdf5892f8890cac
Kepler Vital <kepler.vital@gmail.com>
2019-08-12 18:52:51 +0100

    updated chat bar

--- web/src/app/components/ChatBar.js
+++ web/src/app/components/ChatBar.js
@@ -1,9 +1,8 @@
 import React, { Component } from "react";
 import { StateContext } from '../simplechat/state';
-import { withStyles, Grid } from '@material-ui/core';
+import { withStyles, Grid, Tooltip } from '@material-ui/core';
 import styles from '../styles/chat.module.css';
 import classNames from 'classnames';
-import defaultAvatar from './../images/avatar.jpg'
 
 const useStyles = theme => ({
     
@@ -18,19 +17,17 @@ class ChatBar extends Component {
         const [{chat}, _] = this.context;
         const users = chat.bar.users;
 
-        for(let i = 1; i <= 20; ++i) {
-            //users.push(<Grid item xs={12} className={classNames(styles.user)} key={i}>{i}</Grid>);
-        }
-
         return (
             <Grid container>
                 {chat.bar.directs.map(conversation => {
                     const userInfo = users[conversation.participants.find((uid) => uid != chat.me.id)];
                     return (
-                        <Grid item xs={12} className={classNames(styles.user)} key={conversation.id}>
-                            <div className={classNames(styles.avatar)}></div>
-                            <div className={classNames(styles.userStatus)}></div>
-                        </Grid>
+                        <Tooltip key={conversation.id} title={userInfo.name} placement="left">
+                            <Grid item xs={12} className={classNames(styles.user)}>
+                                <div className={classNames(styles.avatar)}></div>
+                                <div className={classNames(styles.userStatus)}></div>
+                            </Grid>
+                        </Tooltip>
                     );
                 })}
             </Grid>
