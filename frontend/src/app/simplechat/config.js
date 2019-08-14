e6b53576529436f9d54c2e73248341aa54a1a540
Kepler Vital <kepler.vital@gmail.com>
2019-08-14 17:59:12 +0100

    updated config file

--- frontend/src/app/simplechat/config.js
+++ frontend/src/app/simplechat/config.js
@@ -1,5 +1,8 @@
 export default {
     host: 'http://localhost:8080',
+    endpoint: {
+        socket: 'http://localhost:3000'
+    },
     lang: {
         dialog: 'Pick a contact to begin a conversation.',
         type: 'Type yout message here',
