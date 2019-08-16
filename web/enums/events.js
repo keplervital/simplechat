31d5348406e3c8bc3b824903dd5edb7d7ecebbda
Kepler Vital <kepler.vital@gmail.com>
2019-08-16 12:31:14 +0100

    added enums to dispatch events

---
+++ web/enums/events.js
@@ -0,0 +1,7 @@
+module.exports = {
+    DISCONNECT: 'disconnect',
+    MESSAGE_NEW: 'new-message',
+    MESSAGE_SEND: 'send-message',
+    USER_ONLINE: 'set-user',
+    USER_NOTIFY_ONLINE: 'online-users'
+}
